9045bf9f84a8f7eb1eab6d0875dbd5702aeef3e9
jpascale <jpascale@itba.edu.ar>
2018-05-22 13:36:36 -0300

    Fix typo

--- src/encryption.c
+++ src/encryption.c
@@ -26,6 +26,6 @@ InputFile *apply_encryption(InputFile *input_file, EncryptionAlgorithm encryptio
 
 InputFile *apply_decryption(InputFile *input_file, EncryptionAlgorithm encryption)
 {
-  static InputFile *(*dcr_fun_ptr_arr[])(InputFile *) = {echo_encryption};
+  static InputFile *(*dcr_fun_ptr_arr[])(InputFile *) = {echo_decryption};
   return (*dcr_fun_ptr_arr[encryption])(input_file);
 }
